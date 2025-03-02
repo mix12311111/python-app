934f5c46e17fd1a7ca688758fea8e34c9138eef4
xbinh <binhnguyenxuan47@gmail.com>
2025-03-02 16:23:24 +0700

    fix code

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ import sqlite3
 
 def dict_factory(cursor,row):
     d =  {}
-    for idx,col in enumerate ( cursor.description):*
+    for idx,col in enumerate ( cursor.description):
         d[col[0]] =row [idx]
-        return d
+    return d
     
 def connect_db():
     conn = sqlite3.connect('data/database.py')
@@ -25,7 +25,7 @@ def get_user_by_id(id):
     user = cursor.fetchone()
     conn.close()
     return user
- 
+
 def get_user_by_email(email):
     conn = connect_db()
     cursor = conn.cursor()
@@ -34,4 +34,11 @@ def get_user_by_email(email):
     conn.close()
     return user
 
+def get_user_by_email_and_password(email, password):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE email = ? AND password = ?", (email, password))
+    user = cursor.fetchone()
+    conn.close()
+    return user
 print(get_user_by_id(1))
